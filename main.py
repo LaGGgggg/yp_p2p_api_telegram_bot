26fe97efe518a8aeac7c7801a2dbdcdc56db9c3f
alinkalina <alinkameshch@yandex.ru>
2024-04-01 23:20:13 +0300

    feat: команда /help

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from settings import SETTINGS
-from routers import users, p2p_request, p2p_review
+from routers import users, p2p_request, p2p_review, help
 from sql.database import create_all_tables
 
 
@@ -16,7 +16,7 @@ async def main() -> None:
     async def shutdown() -> None:
         await dp.storage.close()
 
-    dp.include_routers(users.router, p2p_request.router, p2p_review.router)
+    dp.include_routers(users.router, p2p_request.router, p2p_review.router, help.router)
 
     bot = Bot(token=SETTINGS.BOT_TOKEN)
 
---
+++ routers/help.py
@@ -0,0 +1,25 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.enums import ParseMode
+
+
+router = Router()
+
+
+@router.message(Command('help'))
+async def send_help_message(message: Message):
+    await message.answer(
+        'Здесь вы можете присылать ссылки на ваши проекты, чтобы их проверили, и самому ревьюить проекты других '
+        'пользователей.  Вот краткая инструкция пользования ботом:\n\n'
+        '<b>Вход</b>\nЧтобы войти в аккаунт, введите команду <code>/login username password</code>, где username - ваш '
+        'логин, а password - ваш пароль.\n\n'
+        '<b>Отправка проекта</b>\nВведите команду /add_p2p_request, далее отправьте ссылку на ваш репозиторий '
+        'с проектом на GitHub.  Затем напишите комментарий, который увидит ревьюер во время проверки.\nПо команде '
+        '/view_requests можно посмотреть список ссылок и комментариев всех ваших отправленных проектов.\n\n'
+        '<b>Ревью</b>\nПо команде /start_review вы получите ссылку на репозиторий проекта на GitHub и комментарий '
+        'создателя.\nВведя команду /complete_review, вы сможете отправить ссылку на ваше ревью.\n'
+        'У вас не может быть сразу несколько ревью одновременно!\n\n'
+        '<b>Выход</b>\nЧтобы выйти из аккаунта, введите команду /logout.',
+        parse_mode=ParseMode.HTML
+    )
